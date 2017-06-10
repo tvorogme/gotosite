2d03d3d667634deb4eb811ffe50332113e108ab3
Andrew Tvorozhkov <tvorog@tvorog.me>
2017-06-10 20:52:38 +0300

    Add really hot fix to add skills in design correctly

--- js/profile/autocomplite.js
+++ js/profile/autocomplite.js
@@ -2,12 +2,34 @@
  * Created by tvorogme on 6/10/17.
  */
 
-function update_autocomplete() {
-    $('.skill').each(function (index) {
-        var critbox = $(this).attr("id");
+function update_autocomplete(needed) {
+    if (needed) {
+        $('.skill').each(function (index) {
+            var critbox = $(this).attr("id");
 
 
-        $('#' + critbox).on("keydown", function (event) {
+            $('#' + critbox).on("keydown", function (event) {
+                if (event.keyCode === $.ui.keyCode.TAB &&
+                    $(this).autocomplete("instance").menu.active) {
+                    event.preventDefault();
+                }
+            }).autocomplete({
+                source: function (request, response) {
+                    $.getJSON("/get_needed_skills", {
+                        skill: request.term
+                    }, response);
+                },
+                focus: function () {
+                    return false;
+                },
+                select: function (event, ui) {
+                    this.value = ui.item.value;
+                    return false;
+                }
+            });
+        });
+    } else {
+        $(needed).on("keydown", function (event) {
             if (event.keyCode === $.ui.keyCode.TAB &&
                 $(this).autocomplete("instance").menu.active) {
                 event.preventDefault();
@@ -26,7 +48,7 @@ function update_autocomplete() {
                 return false;
             }
         });
-    });
+    }
 
 }
 
@@ -34,12 +56,15 @@ var current_input_field_number = 0;
 
 function add_input_field() {
     var html = function (_id) {
-        return '<div id="skill_wrapper" class="col-3"><input class="after_name skill" id="new_skill_%s" placeholder="Скилл" type="text"></div>' % _id
+        // FIXME: Find batter way to do it
+        return '<div id="skill_wrapper" class="col-3"><input class="after_name skill" id="new_skill_' + _id + '" placeholder="Скилл" type="text"></div>'
     };
 
     current_input_field_number += 1;
 
     $("#add_wrapper").before(html(current_input_field_number));
+    update_autocomplete("#new_skill_" + current_input_field_number);
+    console.log(html(current_input_field_number));
 }
 
 $(update_autocomplete());
