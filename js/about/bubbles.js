907af472c78d9d7b4bb8d6766fe8f92627d8d5eb
Andrew Tvorozhkov <tvorog@tvorog.me>
2017-07-08 15:30:50 +0300

    Add borders and resume while scrolling

--- js/about/bubbles.js
+++ js/about/bubbles.js
@@ -10,37 +10,40 @@ function getRandom(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-var photo_urls = ["https://goto.msk.ru/camp_summer/images/comments/nast.jpg", "https://goto.msk.ru/camp_summer/images/comments/maxim.jpg"];
+var photo_urls = ["https://goto.msk.ru/camp_summer/images/comments/nast.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/maxim.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/gridasov.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/zajceva.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/andreeva.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/dimaantonov.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/kabo.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/tsiplenkov.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/sere.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/popkova.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/svatoslav.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/gleb.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/kurilev.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/nast.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/maxim.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/gridasov.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/zajceva.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/andreeva.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/dimaantonov.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/kabo.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/tsiplenkov.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/sere.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/popkova.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/svatoslav.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/gleb.jpg",
+    "https://goto.msk.ru/camp_summer/images/comments/kurilev.jpg"];
 
-var nodes = d3.range(2).map(function (i) {
-    return {radius: getRandom(100, 200), id: i, url: photo_urls[i]};
+var nodes = d3.range(24).map(function (i) {
+    return {radius: getRandom(50, 150), id: i, url: photo_urls[i], padding: 50};
 });
-var nodesCopy = $.extend(true, [], nodes);
-function collide(node) {
-    var r = node.radius + 160;
-    var nx1 = node.x - r;
-    var nx2 = node.x + r;
-    var ny1 = node.y - r;
-    var ny2 = node.y + r;
-    return function (quad, x1, y1, x2, y2) {
-        if (quad.point && (quad.point !== node)) {
-            var x = node.x - quad.point.x;
-            var y = node.y - quad.point.y;
-            var l = Math.sqrt(x * x + y * y);
-            var npr = node.radius + quad.point.radius;
-            if (l < npr) {
-                l = (l - npr) / l * 0.5;
-                x *= l;
-                node.x -= x;
-                y *= l;
-                node.y -= y;
-                quad.point.x += x;
-                quad.point.y += y;
-            }
-        }
-        return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-    };
-}
+
+var maxRadius = 150;
+
+var alpha = .1;
 
 function packup() {
     var pack = d3.layout.pack()
@@ -54,16 +57,11 @@ function packup() {
     svg.selectAll(".node").data(pack.nodes({"children": nodes})
         .filter(function (d) {
             return !d.children;
-        })).enter();
-
-    svg.selectAll(".node").data(pack.nodes({"children": nodes})
-        .filter(function (d) {
-            return !d.children;
         })).enter().append('defs')
         .append('pattern')
         .attr('id', function (d) {
             return (d.id + "-icon");
-        }) // just create a unique id (id comes from the json)
+        })
         .attr('width', 1)
         .attr('height', 1)
         .attr('patternContentUnits', 'objectBoundingBox')
@@ -77,7 +75,6 @@ function packup() {
         .attr("width", 1)
         .attr("preserveAspectRatio", "xMinYMin slice");
 
-
     svg.selectAll(".node")
         .data(pack.nodes({"children": nodes})
             .filter(function (d) {
@@ -89,25 +86,55 @@ function packup() {
         })
         .attr("cx", function (d) {
             return d.x;
-        })
+        }).on("mouseover", function (d) {
+        d.radius += 20;
+        d.padding -= 20;
+        d.r = d.radius;
+        d3.select(this).attr("r", d.radius);
+    }).on("mouseout", function (d) {
+        d.radius -= 20;
+        d.padding += 20;
+        d.r = d.radius;
+        d3.select(this).attr("r", d.radius)
+    })
         .attr("cy", function (d) {
             return d.y;
-        }).on("mouseover", function (d) {
-        d.radius += 10;
-        d3.select(this).attr("r", d.radius)
-    }).on("mouseout", function (d) {
-        d.radius -= 10;
-        d3.select(this).attr("r", d.radius)
-    }).style("fill", function (d) {
-        return ("url(#" + d.id + "-icon-img)");
-    })
-
+        }).style("fill", function (d) {
+        return ("url(#" + d.id + "-icon)")
+    });
 }
 
+function border_check(a, b) {
+    if (b) {
+        if (a > $(window).width()) {
+            a = $(window).width() - 100;
+        } else if (a < 0) {
+            a = 100;
+        }
+        return a
+    } else {
+        if (a < 0) {
+            a = 100;
+        } else if (a > $(window).height()) {
+            a = $(window).height() - 100;
+        }
+        return a
+    }
+}
+
+function isOnScreen(element) {
+    var curPos = element.offset();
+    var curTop = curPos.top;
+    var screenHeight = $(window).height();
+    return (curTop > screenHeight) ? false : true;
+}
+
+var force;
+
 function forceup() {
-    var force = d3.layout.force()
+    force = d3.layout.force()
         .nodes(nodes)
-        .gravity(0.1)
+        .gravity(0.01)
         .charge(0)
         .size([width, height])
         .start();
@@ -120,16 +147,44 @@ function forceup() {
         var n = nodes.length;
 
         while (++i < n) {
-            q.visit(collide(nodes[i]));
+            function set_collide(d) {
+                var r = d.radius + maxRadius + d.padding,
+                    nx1 = d.x - r,
+                    nx2 = d.x + r,
+                    ny1 = d.y - r,
+                    ny2 = d.y + r;
+
+                return function (quad, x1, y1, x2, y2) {
+                    if (quad.point && (quad.point !== d)) {
+                        var x = d.x - quad.point.x,
+                            y = d.y - quad.point.y,
+                            l = Math.sqrt(x * x + y * y),
+                            r = d.radius + quad.point.radius + d.padding;
+                        if (l < r) {
+                            l = (l - r) / l * alpha;
+                            d.x -= x *= l;
+                            d.y -= y *= l;
+                            quad.point.x += x;
+                            quad.point.y += y;
+                        }
+                    }
+                    return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+                }
+            }
+
+            q.visit(set_collide(nodes[i]));
         }
 
         svg.selectAll("circle")
             .attr("cx", function (d) {
+                d.x = border_check(d.x, true);
                 return d.x;
             })
             .attr("cy", function (d) {
+                d.y = border_check(d.y, false);
                 return d.y;
             });
+
     });
 
     d3.selectAll("circle").call(drag);
@@ -137,3 +192,9 @@ function forceup() {
 
 packup();
 forceup();
+
+$(document).scroll(function () {
+    if (isOnScreen($("svg"))) {
+        force.resume();
+    }
+});
