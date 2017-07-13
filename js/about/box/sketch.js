49c2e351bd25ef4a8dddf69411044323011509b4
Andrew Tvorozhkov <tvorog@tvorog.me>
2017-07-13 12:47:06 +0300

    Fix spawn position, now spawn with delay

--- js/about/box/sketch.js
+++ js/about/box/sketch.js
@@ -20,7 +20,6 @@ Array.prototype.random = function () {
 };
 
 var ground;
-
 var mConstraint;
 
 var viewHeight = $(window).height();
@@ -40,32 +39,6 @@ function setup() {
     var canvasmouse = Mouse.create(canvas.elt);
     canvasmouse.pixelRatio = pixelDensity();
 
-
-    for (var i = 0; i < 5; i++) {
-        particles.push(new Particle(viewWidth / 2, viewHeight / 2, viewWidth / Common.random(15, 30), false, {
-            color: colors.random()
-        }));
-
-        storona_kvadrata = viewWidth / Common.random(9, 25);
-        boundaries.push(new Boundary(viewWidth / 2, viewHeight / 2, storona_kvadrata, storona_kvadrata, 0, false, {
-            color: colors.random()
-        }));
-    }
-
-    for (var i = 0; i < 3; i++) {
-        particles.push(new Particle(viewWidth / 2, viewHeight / 2, viewWidth / Common.random(6, 11), false, {
-            color: colors.random(),
-            text: "100 участников"
-        }));
-
-        var storona_kvadrata = viewWidth / Common.random(4, 7);
-        boundaries.push(new Boundary(viewWidth / 2, viewHeight / 2, storona_kvadrata, storona_kvadrata, 0, false, {
-            color: colors.random(),
-            text: "100 участников"
-        }));
-    }
-
-
     var options = {
         mouse: canvasmouse
     };
@@ -75,7 +48,137 @@ function setup() {
 
     mConstraint = MouseConstraint.create(engine, options);
     World.add(world, mConstraint);
-    console.log(mConstraint);
+
+
+    var max_big_shar = 11;
+    var min_big_shar = 6;
+
+    var max_little_shar = 30;
+    var min_little_shar = 15;
+
+    var max_big_boundary = 7;
+    var min_big_boundary = 4;
+
+    var max_little_boundary = 25;
+    var min_little_boundary = 9;
+
+    var max_r = viewWidth / Math.min(min_big_boundary, min_big_shar);
+
+    var first_spawn_x = viewWidth - max_r / 2;
+    var first_spawn_y = max_r / 2;
+
+    var second_spawn_x = max_r / 2;
+    var second_spawn_y = max_r / 2;
+
+
+    var now_spawn_x, now_spawn_y;
+
+    var i1 = 1;
+
+    function myLoop1() {
+        setTimeout(function () {
+            if (i1 % 2 === 0) {
+                now_spawn_x = first_spawn_x;
+                now_spawn_y = first_spawn_y;
+            } else {
+                now_spawn_x = second_spawn_x;
+                now_spawn_y = second_spawn_y;
+            }
+
+            particles.push(new Particle(now_spawn_x, now_spawn_y, viewWidth / Common.random(min_little_shar, max_little_shar), false, {
+                color: colors.random()
+            }));
+            i1++;
+
+            if (i1 < 4) {
+                myLoop1();
+            }
+        }, 1000)
+    }
+
+    myLoop1();
+
+    var i2 = 1;
+
+    function myLoop2() {
+        setTimeout(function () {
+
+            if (i2 % 2 !== 0) {
+                now_spawn_x = first_spawn_x;
+                now_spawn_y = first_spawn_y;
+            } else {
+                now_spawn_x = second_spawn_x;
+                now_spawn_y = second_spawn_y;
+            }
+
+
+            var storona_kvadrata = viewWidth / Common.random(min_little_boundary, max_little_boundary);
+            boundaries.push(new Boundary(now_spawn_x, now_spawn_y, storona_kvadrata, storona_kvadrata, 0, false, {
+                color: colors.random()
+            }));
+
+            i2++;
+            if (i2 < 4) {
+                myLoop2();
+            }
+        }, 1000)
+    }
+
+    myLoop2();
+
+
+    var i3 = 1;
+
+    function myLoop3() {
+        setTimeout(function () {
+            if (i3 % 2 === 0) {
+                now_spawn_x = first_spawn_x;
+                now_spawn_y = first_spawn_y;
+            } else {
+                now_spawn_x = second_spawn_x;
+                now_spawn_y = second_spawn_y;
+            }
+
+            particles.push(new Particle(now_spawn_x, now_spawn_y, viewWidth / Common.random(min_big_shar, max_big_shar), false, {
+                color: colors.random(),
+                text: "100 участников"
+            }));
+            i3++;
+            if (i3 < 2) {
+                myLoop3();
+            }
+        }, 1000)
+    }
+
+    myLoop3();
+
+    var i = 1;
+
+    function myLoop() {
+        setTimeout(function () {
+
+            if (i % 2 !== 0) {
+                now_spawn_x = first_spawn_x;
+                now_spawn_y = first_spawn_y;
+            } else {
+                now_spawn_x = second_spawn_x;
+                now_spawn_y = second_spawn_y;
+            }
+
+            var storona_kvadrata = viewWidth / Common.random(min_big_boundary, max_big_boundary);
+            boundaries.push(new Boundary(now_spawn_x, now_spawn_y, storona_kvadrata, storona_kvadrata, 0, false, {
+                color: colors.random(),
+                text: "100 участников"
+            }));
+
+            i++;
+            if (i < 3) {
+                myLoop();
+            }
+        }, 1000)
+    }
+
+    myLoop();
 }
 
 
