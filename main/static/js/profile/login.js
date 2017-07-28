e065fb9c14e272f29d7a55eb1a76e6ef07a002ab
Andrew Tvorozhkov <tvorog@tvorog.me>
2017-07-28 20:47:52 +0300

    Add reginster in login box

--- main/static/js/profile/login.js
+++ main/static/js/profile/login.js
@@ -9,3 +9,14 @@ function toggle_login_placeholder() {
     $("#login_placeholder_table").toggle();
     login_placeholder_state = !login_placeholder_state
 }
+
+function display_login_placeholder(n) {
+
+    if (n === 2) {
+        $("#login_placeholder_first_page").css('display', 'none');
+        $("#login_placeholder_second_page").css('display', 'block');
+    } else if (n === 1) {
+        $("#login_placeholder_first_page").css('display', 'block');
+        $("#login_placeholder_second_page").css('display', 'none');
+    }
+}
