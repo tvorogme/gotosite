82290d41d519f08fb6c962b276dec8dee28e5960
Andrew Tvorozhkov <tvorog@tvorog.me>
2017-08-20 19:03:32 +0300

    Add new styles for shop

--- gotosite/settings.py
+++ gotosite/settings.py
@@ -11,17 +11,10 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from .settings_secret import *
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u4ycjix$7b)u*(2_nl$hc5f*cka)n_3cw+hu(q+3-by$kn@801'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['goto.msk.ru', 'localhost']
