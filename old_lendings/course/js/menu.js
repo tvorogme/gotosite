7972ad463c004d0b8906a46a50f1ed03da4bfb26
Andrew Tvorozhkov <tvorog@tvorog.me>
2017-10-05 15:08:10 +0300

    Add stat course

---
+++ old_lendings/course/js/menu.js
@@ -0,0 +1,34 @@
+/**
+ * Created by xenx on 1/31/17.
+ */
+
+$(document).ready(function (ev) {
+    var toggle = $('#ss_toggle');
+    var menu = $('#ss_menu');
+    var rot;
+
+    $('#ss_toggle').on('click', function (ev) {
+        rot = parseInt($(this).data('rot')) - 180;
+        menu.css('transform', 'rotate(' + rot + 'deg)');
+        menu.css('webkitTransform', 'rotate(' + rot + 'deg)');
+        if ((rot / 180) % 2 == 0) {
+            //Moving in
+            toggle.parent().addClass('ss_active');
+            toggle.addClass('close');
+        } else {
+            //Moving Out
+            toggle.parent().removeClass('ss_active');
+            toggle.removeClass('close');
+        }
+        $(this).data('rot', rot);
+    });
+
+    menu.on('transitionend webkitTransitionEnd oTransitionEnd', function () {
+        if ((rot / 180) % 2 == 0) {
+            $('#ss_menu div i').addClass('ss_animate');
+        } else {
+            $('#ss_menu div i').removeClass('ss_animate');
+        }
+    });
+
+});
